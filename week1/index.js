53a7538b1e0368d89edea2a16727f1064dc24ae1
What-Question-Mark <97435840+What-Question-Mark@users.noreply.github.com>
2022-10-06 20:39:29 +1000

    Update index.js

--- week1/index.js
+++ week1/index.js
@@ -17,6 +17,40 @@ process.stdin.on('keypress', (ch, key) => {
       yindex = yindex - 1;
       if (xindex <= 0){xindex=1}
       if (yindex <= 0){yindex=1}
+      if (level == 1){
+        if (xindex == 2 && yindex == 1) {xindex = 1;yindex=1}
+        if (xindex == 2 && yindex == 2) {xindex = 1;yindex=1}
+        if (xindex == 3 && yindex == 2) {xindex = 1;yindex=1}
+        if (xindex == 4 && yindex == 2) {xindex = 1;yindex=1}
+        if (xindex == 5 && yindex == 2) {xindex = 1;yindex=1}
+        if (xindex == 6 && yindex == 2) {xindex = 1;yindex=1}
+        if (xindex == 7 && yindex == 2) {xindex = 1;yindex=1}
+        if (xindex == 8 && yindex == 2) {xindex = 1;yindex=1}
+        if (xindex == 9 && yindex == 2) {xindex = 1;yindex=1}
+
+        if (xindex == 1 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 2 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 3 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 4 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 5 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 6 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 7 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 8 && yindex == 4) {xindex = 1;yindex=1}
+
+        if (xindex == 8 && yindex == 3) {xindex = 1;yindex=1}
+        if (xindex == 8 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 8 && yindex == 5) {xindex = 1;yindex=1}
+        if (xindex == 8 && yindex == 6) {xindex = 1;yindex=1}
+
+        if (xindex == 10 && yindex == 3) {xindex = 1;yindex=1}
+        if (xindex == 10 && yindex == 4) {xindex = 1;yindex=1}
+        if (xindex == 10 && yindex == 5) {xindex = 1;yindex=1}
+        if (xindex == 10 && yindex == 6) {xindex = 1;yindex=1}
+      }
+      if (level == 2){
+      }
+      if (level == 3){
+      }
       levels(level, xindex, yindex)
       if (dev == 1) {console.log(`Coords: \x1b[93mX:${xindex},  Y:${yindex}\x1b[37m`)}
     }
